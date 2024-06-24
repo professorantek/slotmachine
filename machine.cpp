ac5ad591c95ecce6d69a073072ac6d56dbfd9f24
polykacz gowna <achicken.mcbite@gmail.com>
2024-06-24 22:28:57 +0200

    New mechanics

--- machine.cpp
+++ machine.cpp
@@ -24,6 +24,8 @@ void Machine::Build(int n, int X, int Y){
 }
 void Machine::DrawMachine(SDL_Renderer *r, TTF_Font *f){
     SDL_Rect *rect = new SDL_Rect;
+
+    
     for(Column *c: columns){
         for(Slot *s: c->SetVisible()){
             rect->h = SLOTH*scale;
@@ -34,6 +36,8 @@ void Machine::DrawMachine(SDL_Renderer *r, TTF_Font *f){
             
         }
     }
+
+
     rect->h = 520*scale;
     rect->w = 230*scale;
     rect->x = x*scale;
@@ -41,23 +45,26 @@ void Machine::DrawMachine(SDL_Renderer *r, TTF_Font *f){
     SDL_RenderCopy(r, machineTex, NULL, rect);
 
 
-
     rect->h = 50*scale;
     rect->w = 100*scale;
     rect->x = x + 65*scale;
     rect->y = y + 400*scale;
     std::string balanceStr = std::to_string(balance);
+    balanceStr = balanceStr.substr(0, balanceStr.size()-4);
     char* balanceChar = &balanceStr[0];  
     SDL_Surface *balanceSurf = TTF_RenderText_Solid(f, balanceChar, {255,255,255,255});
     SDL_Texture *balanceText = SDL_CreateTextureFromSurface(r, balanceSurf);
     SDL_RenderCopy(r, balanceText, NULL, rect);
     SDL_FreeSurface(balanceSurf);
     SDL_DestroyTexture(balanceText);
+
+
     rect->h = 50*scale;
     rect->w = 200*scale;
     rect->x = x + 15*scale;
     rect->y = y + 10*scale;
     std::string betStr = "Bet:" + std::to_string(bet);
+    betStr = betStr.substr(0, betStr.size()-4);
     char* betChar = &betStr[0];
     SDL_Surface *betSurf = TTF_RenderText_Solid(f, betChar, {255,255,255,255});
     SDL_Texture *betText = SDL_CreateTextureFromSurface(r, betSurf);
@@ -79,28 +86,48 @@ void Machine::Spin(bool trytostop){
         }
     }
 }
-bool Machine::DidWin(){
+void Machine::ColorSlots(SDL_Color c, std::vector<Slot> v, SDL_Renderer *r){
+    for(Slot s: v){
+        SDL_Rect rect{s.x*scale, s.y*scale, SLOTW*scale, SLOTH*scale};
+        SDL_SetRenderDrawColor(r, c.r, c.g, c.b, c.a);
+        SDL_RenderFillRect(r, &rect);
+    }
+    SDL_RenderPresent(r);
+}
+std::vector<Slot> Machine::DidWin(double *amount){
+    std::vector<Slot> empty, winners;
     if(running){
-        return false;
+        return empty;
     }
-    int t = -1;
+    
+    std::vector<std::vector<Slot>> t;
     for(Column *c: columns){
-        Slot *s = c->CenterSlot();
-        
-        if(s==nullptr){
-            return false;
-        }
-        if(t ==-1 ){
-            t = s->type;
-            
-        }
-        if(t!=s->type){
-            balance -= bet;
-            return false;
-        }
+        t.push_back(c->WinningSlots());
     }
-    balance += bet*10;
-    return true;
+    *amount = 0;
+    if(t[0][0].type==t[1][0].type&&t[0][0].type==t[2][0].type){
+        *amount = bet*pow(2, t[0][0].type);
+        winners = {t[0][0], t[1][0], t[2][0]};
+        winners.push_back(t[0][0]);
+        winners.push_back(t[1][0]);
+        winners.push_back(t[2][0]);
+    }
+    if(t[0][1].type==t[1][1].type&&t[0][1].type==t[2][1].type){
+        if(*amount!=0)*amount *= bet*pow(2, t[0][1].type);
+        else *amount = bet*pow(2, t[0][1].type);
+        winners.push_back(t[0][1]);
+        winners.push_back(t[1][1]);
+        winners.push_back(t[2][1]);
+    }
+    if(t[0][2].type==t[1][2].type&&t[0][2].type==t[2][2].type){
+        if(*amount!=0)*amount *= bet*pow(2, t[0][2].type);
+        else *amount = bet*pow(2, t[0][2].type);
+        winners.push_back(t[0][2]);
+        winners.push_back(t[1][2]);
+        winners.push_back(t[2][2]);
+    }
+    balance += *amount;
+    return winners;
 }
 bool Machine::IsRunning(){
     return running;
